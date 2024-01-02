0045eabe59748c109542d1bb16b8b2ca11ada79c
Miguel Martínez <leugimmanre@gmail.com>
2024-01-02 20:18:26 +0100

    Netlit¡fy

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://codeshirts-backend.onrender.com'
+    // baseURL: 'http://localhost:4000'
 });
 
 export default axiosClient;
